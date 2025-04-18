5d1561e424431257c0b6829eec03803d5188c3ba
Pawcharz <pawel030724@gmail.com>
2025-04-18 09:21:41 +0200

    wip: Add scanning of the front page of id

--- App.tsx
+++ App.tsx
@@ -2,7 +2,25 @@ import React from 'react';
 import { launchImageLibrary } from 'react-native-image-picker';
 
 import {SafeAreaView, Text, StyleSheet, View, Button} from 'react-native';
-import TextRecognition from '@react-native-ml-kit/text-recognition';
+import TextRecognition, { TextRecognitionResult } from '@react-native-ml-kit/text-recognition';
+
+interface EmiratesIdCardFrontInfo {
+  name: string,
+  idNumber: string,
+  nationality: string,
+}
+
+interface EmiratesIdCardBackInfo {
+  sex: string,
+  dateOfBirth: Date
+  issueDate: Date
+  expiryDate: Date
+}
+
+const ID_CARD_NUMBER_REGEX = new RegExp(/784[-]*\d{4}[-]*\d{7}[-]*\d/);
+// All in lower case
+const ID_CARD_NATIONALITY_PREFIX = 'nationality:';
+const ID_CARD_NAME_PREFIX = 'name:';
 
 const App = () => {
 
@@ -16,7 +34,78 @@ const App = () => {
 
       // Use directly with ML Kit:
       const recognitionResult = await TextRecognition.recognize(asset.uri);
-      console.log('Recognized text:', recognitionResult.text);
+      console.log('Recognized text:', recognitionResult);
+    } else {
+      console.log('No image selected.');
+    }
+  };
+
+  const extractDataFromIdFront = (ocrResult: TextRecognitionResult): EmiratesIdCardFrontInfo | null => {
+
+    // Check is it United Arab Emirates
+    if (!ocrResult.text.toLowerCase().includes('united arab emirates')) {
+
+      return null;
+    }
+
+    const name = ocrResult.blocks.find(block => block.text.toLowerCase().includes(ID_CARD_NAME_PREFIX))?.text.split(ID_CARD_NAME_PREFIX)?.[1] || '';
+
+    const idNumber = ocrResult.text.match(ID_CARD_NUMBER_REGEX)?.[0] || '';
+
+    const nationality = ocrResult.blocks.find(block => block.text.toLowerCase().includes(ID_CARD_NATIONALITY_PREFIX))?.text.toLowerCase().split(ID_CARD_NATIONALITY_PREFIX)?.[1].trim() || '';
+
+    const frontInfo: EmiratesIdCardFrontInfo = {
+      name,
+      idNumber,
+      nationality,
+    };
+
+    return frontInfo;
+  };
+
+  const extractDataFromIdBack = (ocrResult: TextRecognitionResult): EmiratesIdCardBackInfo | null => {
+    console.log(ocrResult);
+
+    const backInfo: EmiratesIdCardBackInfo = {
+      sex: '',
+      dateOfBirth: new Date(),
+      issueDate: new Date(),
+      expiryDate: new Date(),
+    };
+
+    return backInfo;
+  };
+
+  const pickAndRecognizeUAEIdCard = async () => {
+
+    // // Get front of the id card
+    // const frontResult = await launchImageLibrary({ mediaType: 'photo' });
+
+    // if (frontResult.assets && frontResult.assets.length > 0) {
+    //   const asset = frontResult.assets[0];
+
+    //   // Use directly with ML Kit:
+    //   const recognitionResult = await TextRecognition.recognize(asset.uri);
+
+    //   const idCardFrontInfo = extractDataFromIdFront(recognitionResult);
+
+    //   console.log('idCardFrontInfo: ', idCardFrontInfo);
+    // } else {
+    //   console.log('No image selected.');
+    // }
+
+    // Get back of the id card
+    const backResult = await launchImageLibrary({ mediaType: 'photo' });
+
+    if (backResult.assets && backResult.assets.length > 0) {
+      const asset = backResult.assets[0];
+
+      // Use directly with ML Kit:
+      const recognitionResult = await TextRecognition.recognize(asset.uri);
+
+      const idCardBackInfo = extractDataFromIdBack(recognitionResult);
+
+      console.log('idCardBackInfo: ', idCardBackInfo);
     } else {
       console.log('No image selected.');
     }
@@ -27,7 +116,7 @@ const App = () => {
       <View style={styles.card}>
         <Text style={styles.title}>MLKit Demo App</Text>
         <Text style={styles.subtitle}>Welcome to the future of mobile AI ✨</Text>
-        <Button title="test" onPress={pickAndRecognize} />
+        <Button title="test" onPress={pickAndRecognizeUAEIdCard} />
       </View>
     </SafeAreaView>
   );
